dbb827c44f95ab26383d75bd7d4e59d544532b09
pradeep531988 <pradeep.inbox@rediffmail.com>
2019-01-08 18:49:46 +0530

    Updated code for Spring Config client code

---
+++ src/main/java/com/cloud/configclient/SpringConfigClientApplication.java
@@ -0,0 +1,16 @@
+package com.cloud.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+//@EnableWebSecurity
+@SpringBootApplication
+public class SpringConfigClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConfigClientApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/cloud/configclient/controller/HomeController.java
@@ -0,0 +1,20 @@
+package com.cloud.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class HomeController
+{
+  @Value("${name}")
+  String name;
+ 
+  @GetMapping("/name")
+  public String name()
+  {
+    return name;
+  }
+}
